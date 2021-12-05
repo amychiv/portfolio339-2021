6943c6d79a6485995fa228ceb2c0a2ee015eb2ec
amychiv <89854686+amychiv@users.noreply.github.com>
2021-12-05 12:32:32 -0500

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,21 @@
+var count = 0
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+document.querySelector(".menu_icon").addEventListener("click", function() {
+  count += 1;
+  if (count % 2 != 0) {
+    document.getElementById("dropdown").className = "newclass"
+    document.getElementById("main-order-button").style.visibility = "hidden";
+  }
+  else {
+    document.getElementById("dropdown").className = "oldclass"
+    document.getElementById("main-order-button").style.display = "flex";
+  }
+  console.log("Menu should appear and disappear");
+});
+
+
+
